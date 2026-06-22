6c118c2de805d3c0318eccf5c5590d50ee4b795b
Guilherme Pessoa Lima Diniz <glimadiniz2@gmail.com>
2026-06-22 18:37:11 -0300

    feat: implementar model de Treino

---
+++ src/models/Treino.js
@@ -0,0 +1,48 @@
+const db = require('../config/database');
+
+class Treino {
+
+    static listarTodos(callback) {
+
+        const sql = `
+            SELECT
+                treino.id,
+                treino.nome,
+                treino.data_criacao,
+                treino.observacoes,
+                cliente.nome AS cliente,
+                personal_trainer.nome AS personal
+            FROM treino
+            INNER JOIN cliente
+                ON treino.cliente_id = cliente.id
+            INNER JOIN personal_trainer
+                ON treino.personal_id = personal_trainer.id
+        `;
+
+        db.query(sql, callback);
+    }
+
+    static criar(dados, callback) {
+
+        const sql = `
+            INSERT INTO treino
+            (nome, data_criacao, observacoes, cliente_id, personal_id)
+            VALUES (?, ?, ?, ?, ?)
+        `;
+
+        db.query(
+            sql,
+            [
+                dados.nome,
+                dados.data_criacao,
+                dados.observacoes,
+                dados.cliente_id,
+                dados.personal_id
+            ],
+            callback
+        );
+    }
+
+}
+
+module.exports = Treino;
