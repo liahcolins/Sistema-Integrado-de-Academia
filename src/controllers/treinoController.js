b97daab3fef4a4eb08d2d63035d58dfeb10adfb1
Guilherme Pessoa Lima Diniz <glimadiniz2@gmail.com>
2026-06-22 21:09:14 -0300

    feat: criar controller de Treino

---
+++ src/controllers/treinoController.js
@@ -0,0 +1,42 @@
+const Treino = require('../models/Treino');
+
+class TreinoController {
+
+    static listar(req, res) {
+
+        Treino.listarTodos((erro, resultados) => {
+
+            if (erro) {
+                return res.status(500).json({
+                    erro: 'Erro ao buscar treinos'
+                });
+            }
+
+            res.json(resultados);
+
+        });
+
+    }
+
+    static criar(req, res) {
+
+        Treino.criar(req.body, (erro, resultado) => {
+
+            if (erro) {
+                return res.status(500).json({
+                    erro: 'Erro ao cadastrar treino'
+                });
+            }
+
+            res.status(201).json({
+                mensagem: 'Treino cadastrado com sucesso',
+                id: resultado.insertId
+            });
+
+        });
+
+    }
+
+}
+
+module.exports = TreinoController;
