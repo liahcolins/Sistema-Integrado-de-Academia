e478188ffe1cf458dbafb53740942b848b5f95de
liahcolins <liahcolins@gmail.com>
2026-06-23 19:16:33 -0300

    feat: implementar gerenciamento de clientes e personais

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'senha', //alterar com a senha individual do bd de vocês
+    password: '051123', //alterar com a senha individual do bd de vocês
     database: 'academia'
 });
 
